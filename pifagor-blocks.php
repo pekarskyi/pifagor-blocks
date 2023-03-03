fe973c73be341caa8574348859db07089357ae80
Mykola Pekarskyi <dizer7@gmail.com>
2023-03-03 14:03:49 +0200

    Update pifagor-blocks.php

--- pifagor-blocks.php
+++ pifagor-blocks.php
@@ -15,6 +15,12 @@
  * Domain Path:       /languages
  */
 
+define( 'PB_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+require_once( PB_PLUGIN_PATH . 'inc/ACF_Field_Unique_ID.php' );
+PhilipNewcomer\ACF_Unique_ID_Field\ACF_Field_Unique_ID::init();
+
 //Check active plugin ACF
 require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if (!is_plugin_active('advanced-custom-fields-pro/acf.php')) {
